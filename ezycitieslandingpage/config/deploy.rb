ab6eb86218d92dd1a5b21a01834096537fe51c1c
Francisco Lopes <1130576@isep.ipp.pt>
2016-04-28 21:19:34 +0100

    pokjpo

--- ezycitieslandingpage/config/deploy.rb
+++ ezycitieslandingpage/config/deploy.rb
@@ -1,15 +1,15 @@
 lock '3.5.0'
 
-set :application, 'contactbook'
+set :application, 'ezycitieslandingpage'
 set :repo_url, 'git@github.com:franciscoml67/EzyCities.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/ezycitieslandingpage'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'jruby-1.7.19' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-2.3.0' # Edit this if you are using MRI Ruby
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
