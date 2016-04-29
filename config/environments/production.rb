5c93128d7445fa3b46c4a39785dd6b98caa7b2ca
Rafael Chaves <1120677@isep.ipp.pt>
2016-04-30 00:02:17 +0100

    asdasd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 		domain:               'gmail.com',
 		user_name:            'fmldeveloping@gmail.com',
 		password:             'SonsOfAnarchy1967.', #stepbystep8891
-		authentication:       'login',
+		authentication:       'plain',
 		enable_starttls_auto: true
   }
 end
