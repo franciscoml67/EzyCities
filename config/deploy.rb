74168b5b0dd5cfbcf4deffcb8fc37da89518b729
Rafael Chaves <1120677@isep.ipp.pt>
2016-04-29 22:36:50 +0100

    asdasd

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.2.4' # Edit this if you are using MRI Ruby
+set :assets_roles, [:web, :app]   
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
