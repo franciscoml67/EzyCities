8d6ce7bc7d3e0321584c5616a26a026b38265563
Rafael Chaves <1120677@isep.ipp.pt>
2016-04-30 00:00:41 +0100

    asdasd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
 		address:              'smtp.gmail.com',
-		port:                 587,
+		port:                 465,
 		domain:               'gmail.com',
 		user_name:            'fmldeveloping@gmail.com',
 		password:             'SonsOfAnarchy1967.', #stepbystep8891
